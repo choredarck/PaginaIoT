76700bbeb85dc23af765006ce4efffe8d3818fe7
ChoreD <luis-nuro@live.com.mx>
2020-11-02 22:14:40 -0600

    conf4

--- pruebita/settings.py
+++ pruebita/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'pruebita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],#'C:/Users/Chore-Darc/Desktop/CUCEI/Projects Django/pruebita/pruebita/templates/'
+        'DIRS': ['pruebita/pruebita/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
